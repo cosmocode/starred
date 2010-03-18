a0ba5e379b9bc12e2de8a55b9655532f948b1596
Dominik Eckelmann <eckelmann@cosmocode.de>
2010-03-18 15:52:32 +0100

    typo fix

--- lang/de/lang.php
+++ lang/de/lang.php
@@ -7,7 +7,7 @@
 
 
 $lang['star_on']  = 'Gemerkt - klicken, um diese Seite zu vergessen.';
-$lang['star_off'] = 'Nicht gemerkt - klickem, um diese Seite zu merken.';
+$lang['star_off'] = 'Nicht gemerkt - klicken, um diese Seite zu merken.';
 $lang['login']    = 'Bitte loggen Sie sich ein, um Ihre gemerkten Seiten zu sehen.';
 $lang['none']     = 'Sie haben momentan keine gemerkten Seiten.';
 $lang['e_nosqlite'] = 'Das sqlite-Helper-Plugin wurde nicht gefunden.';
